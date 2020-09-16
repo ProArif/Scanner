6fc0daa25134470f74d75ae47f0694ce1b22c11b
Md Ariful Islam <34057291+ProArif@users.noreply.github.com>
2020-09-16 19:59:37 +0600

    Text recognizer added and implemented primarily.

--- app/src/main/java/com/nodeers/scanner/MainActivity.java
+++ app/src/main/java/com/nodeers/scanner/MainActivity.java
@@ -1,14 +1,171 @@
 package com.nodeers.scanner;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_camera;
+    private Button btn_gallery;
+    private Uri imgUri;
+    private LinearLayout layout;
+
+    private InputImage image;
+    private TextRecognizer recognizer;
+
+    private TextView tvResult;
+
+    Task<Text> result;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recognizer = TextRecognition.getClient();
+
+        layout = (LinearLayout) findViewById(R.id.choice_list);
+        tvResult = findViewById(R.id.resultText);
+
+        btn_camera = findViewById(R.id.camera);
+        btn_gallery = findViewById(R.id.gallery);
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("clicked","btn scan on click");
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(MainActivity.this);
+            }
+        });
+
+        btn_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity(imgUri)
+                        .start(MainActivity.this);
+            }
+        });
+        
     }
+
+    private void openDialog() {
+
+//        Log.e("clicked","entered opedDialog");
+//
+//
+//
+//        TextView tv_camera = layout.findViewById(R.id.cam);
+//        TextView tv_gallery = layout.findViewById(R.id.gallery);
+//
+//        tv_camera.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//            }
+//        });
+
+//        tv_gallery.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//
+//            }
+//        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+
+
+                try {
+                    image = InputImage.fromFilePath(this, resultUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                processImage();
+
+                Log.e("get the image", String.valueOf(resultUri));
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
+    private void processImage() {
+
+        result = recognizer.process(image)
+                .addOnSuccessListener(new OnSuccessListener<Text>() {
+            @Override
+            public void onSuccess(Text text) {
+                String resultText = text.getText();
+                for (Text.TextBlock block : text.getTextBlocks()) {
+                    String blockText = block.getText();
+                    Point[] blockCornerPoints = block.getCornerPoints();
+                    Rect blockFrame = block.getBoundingBox();
+                    for (Text.Line line : block.getLines()) {
+                        String lineText = line.getText();
+                        Point[] lineCornerPoints = line.getCornerPoints();
+                        Rect lineFrame = line.getBoundingBox();
+                        for (Text.Element element : line.getElements()) {
+                            String elementText = element.getText();
+                            Point[] elementCornerPoints = element.getCornerPoints();
+                            Rect elementFrame = element.getBoundingBox();
+                        }
+                    }
+                }
+
+                tvResult.setText(resultText);
+
+
+                Toast.makeText(MainActivity.this,
+                        " processing image successfull",Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,
+                                "Failed to process the image",Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
 }
